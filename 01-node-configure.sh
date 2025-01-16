a758c9479b844856f90573fd0d2e002522e24f2c
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2025-01-16 11:28:03 +0900

    Update 01-node-configure.sh

--- 01-node-configure.sh
+++ 01-node-configure.sh
@@ -51,7 +51,7 @@ EOF
 modprobe br_netfilter
 modprobe overlay
 
-cat <<EOF>> /etc/hosts
+cat <<EOF> /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
 192.168.10.10 node1.example.com node1
