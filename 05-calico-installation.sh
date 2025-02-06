ebd83e4284dadef10dce7950e774f182a6c017af
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2025-02-06 09:35:26 +0900

    Update 05-calico-installation.sh

--- 05-calico-installation.sh
+++ 05-calico-installation.sh
@@ -25,5 +25,5 @@ EOF
 helm repo add projectcalico https://docs.tigera.io/calico/charts
 kubectl create namespace tigera-operator
 helm install calico projectcalico/tigera-operator --version v3.27.5 --namespace tigera-operator
-kubectl taint node node1.example.com node-role.kubernetes.io/control-plane:NoSchedule-
+kubectl taint node controller1.example.com node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl apply -f calico-quay-crd.yaml
