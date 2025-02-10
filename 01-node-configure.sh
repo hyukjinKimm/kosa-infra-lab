9617a527fb9e3a7165f79ce4d1f3cc17cb0fd260
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2025-02-10 17:06:24 +0900

    Update 01-node-configure.sh

--- 01-node-configure.sh
+++ 01-node-configure.sh
@@ -54,12 +54,11 @@ modprobe overlay
 cat <<EOF> /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-192.168.0.10 controller1.example.com controller1
-192.168.0.11 controller2.example.com controller2
-192.168.0.12 controller3.example.com controller3
-192.168.0.20 worker1.example.com worker1
-192.168.0.30 worker2.example.com worker2
-192.168.0.40 storage.example.com storage
-192.168.0.200 lb.example.com
+192.168.100.101 controller1.example.com controller1
+192.168.100.102 controller2.example.com controller2
+192.168.100.103 controller3.example.com controller3
+192.168.100.104 worker1.example.com worker1
+192.168.100.105 worker2.example.com worker2
+192.168.100.150 lb.example.com
 EOF
 
